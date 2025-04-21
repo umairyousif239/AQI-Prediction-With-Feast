28fbec2de69415470990a24da1aac6b981058876
Umair Yousif <37881515+umairyousif239@users.noreply.github.com>
2025-04-21 22:53:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import datetime as dt
 import requests
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api = os.getenv("open_weather_api")
-print(api)
+AQI_API = os.getenv("AQI_API")
+Weather_API = os.getenv("Weather_API")
+
+response = requests.get(AQI_API).json()
+response2 = requests.get(Weather_API).json()
+
+print(response)
+print(response2)
